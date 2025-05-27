d0046401f4b861db6820da10ae71e1831cf550a4
Akemisora <akemisora040503@gmail.com>
2025-05-27 20:07:13 +0700

    fuzzy operator

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <cmath>
+
+double tnorm(double a, double b);
+double snorm(double a, double b);
+double complement(double a);
+double implication(double a, double b);
+
+double tnorm(double a, double b) {
+	return fmin(a, b);
+}
+
+double snorm(double a, double b) {
+	return fmax(a, b);
+}
+
+double complement(double a) {
+	return 1 - a;
+}
+
+
 
 int main()
 {
