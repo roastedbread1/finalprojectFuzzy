336a2bbdf7e92ba297e3109163f65f685ca2aa92
Akemisora <akemisora040503@gmail.com>
2025-05-27 20:15:28 +0700

    membership function template

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,47 @@ double complement(double a) {
 	return 1 - a;
 }
 
+// Jarak terhadap sesuatu di depan mobil.
+// Input: x dalam meter.
+double membership_jarak_dekat(double x) {
+	return 0;
+}
+
+double membership_jarak_sedang(double x) {
+	return 0;
+}
+
+double membership_jarak_jauh(double x) {
+	return 0;
+}
+
+double membership_jarak_jauuuuuh(double x) {
+	return 0;
+}
+
+// Kecepatan mobil saat ini.
+// Input: x dalam meter / detik.
+
+double membership_kecepatan_berhenti(double x) {
+	return 0;
+}
+
+double membership_kecepatan_lambat(double x) {
+	return 0;
+}
+
+double membership_kecepatan_sedang(double x) {
+	return 0;
+}
+
+double membership_kecepatan_cepat(double x) {
+	return 0;
+}
+
+
+
+
+
 
 
 int main()
